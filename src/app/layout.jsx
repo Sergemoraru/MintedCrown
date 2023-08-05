a2b456a5673b7d36efbcd6ab6c2e326ef567ab10
Serge Moraru <sergemoraru@gmail.com>
2023-08-05 15:54:56 -0400

    initial commit

---
+++ src/app/layout.jsx
@@ -0,0 +1,40 @@
+import { Inter, Lexend } from 'next/font/google'
+import clsx from 'clsx'
+
+import '@/styles/tailwind.css'
+
+export const metadata = {
+  title: {
+    template: 'MCM - Minted Crown Marketing',
+    default: 'MCM - Reign Your Market with Minted Crown!',
+  },
+  description:
+    "Where creativity rules marketing. Your brand's crowning achievement awaits!",
+}
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+})
+
+const lexend = Lexend({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-lexend',
+})
+
+export default function RootLayout({ children }) {
+  return (
+    <html
+      lang="en"
+      className={clsx(
+        'h-full scroll-smooth bg-white antialiased',
+        inter.variable,
+        lexend.variable
+      )}
+    >
+      <body className="flex h-full flex-col">{children}</body>
+    </html>
+  )
+}
---
+++ src/app/not-found.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/no-unescaped-entities */
+import Link from 'next/link'
+
+import { Button } from '@/components/Button'
+import { Logo } from '@/components/Logo'
+import { SlimLayout } from '@/components/SlimLayout'
+
+export default function NotFound() {
+  return (
+    <SlimLayout>
+      <div className="flex">
+        <Link href="/" aria-label="Home">
+          {/* <Logo className="h-10 w-auto" /> */}
+        </Link>
+      </div>
+      <p className="mt-20 text-sm font-medium text-gray-700">404</p>
+      <h1 className="mt-3 text-lg font-semibold text-gray-900">
+        Page not found
+      </h1>
+      <p className="mt-3 text-sm text-gray-700">
+        Oopsie-daisy! 
+        Looks like the page you're searching for went on a wild quest through the digital kingdom and got lost in the enchanted forest of bits and bytes. 
+        Fear not, though; our team of tech-savvy wizards is already on the case to guide you back to the marketing magic you seek! 
+        In the meantime, why not enjoy a cup of virtual tea while we unravel this delightful mystery? 🧙‍♂️🌳✨
+      </p>
+      <Button href="/" className="mt-10">
+        Go back home
+      </Button>
+    </SlimLayout>
+  )
+}
