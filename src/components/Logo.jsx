b542b5279f38fbc7fc0c4a4e3619580ef1d5b67e
Serge Moraru <sergemoraru@gmail.com>
2023-08-05 15:54:45 -0400

    initial commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ReactComponent as Logo } from '../images/logos/logo1.svg' // Adjust the path to your SVG file
+
+const LogoComponent = () => (
+  <div>
+    <Logo width="100" height="40" />
+  </div>
+)
+
+export default LogoComponent
