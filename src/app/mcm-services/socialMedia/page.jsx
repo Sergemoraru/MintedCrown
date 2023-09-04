48da9516bd97126c40492d78e6638445d7a934a8
Serge Moraru <sergemoraru@gmail.com>
2023-09-04 13:22:32 -0400

    changed file names

--- src/app/mcm-services/socialMedia/page.jsx
+++ src/app/mcm-services/socialMedia/page.jsx
@@ -4,7 +4,7 @@ import socialMedia from '@/images/friends-using-smartphones-together-outdoors.jp
 
 import Image from 'next/image'
 import { Footermin } from '@/components/Footermin'
-import { Headermin } from '@/components/headermin'
+import { headermin } from '@/components/headermin'
 
 export default function StrategyAndAdvisory() {
   return (
@@ -41,7 +41,7 @@ export default function StrategyAndAdvisory() {
         </svg>
       </div>
       <div className="-mb-12 -mt-24">
-        <Headermin />
+        <headermin />
       </div>
 
       <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-5 lg:pt-20">
